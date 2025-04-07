886d5230b39b2ee60df9e91c63b8514fcd92270d
Paulo Oliveira <paulo.oliveira@8x8.com>
2025-04-07 12:45:40 +0100

    spring: add new spring service

---
+++ maven-spring/src/main/java/com/poliveira/javaverse/JavaverseApplication.java
@@ -0,0 +1,13 @@
+package com.poliveira.javaverse;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JavaverseApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JavaverseApplication.class, args);
+	}
+
+}
---
+++ maven-spring/src/main/java/com/poliveira/javaverse/controller/TaskController.java
@@ -0,0 +1,45 @@
+package com.poliveira.javaverse.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/tasks")
+public class TaskController {
+
+    // Create Task
+    public void createTask() {
+        // Implementation for creating a task
+    }
+
+    // Read Task
+    public void readTask() {
+        // Implementation for reading a task
+    }
+
+    // Update Task
+    public void updateTask() {
+        // Implementation for updating a task
+    }
+
+    // Delete Task
+    public void deleteTask() {
+        // Implementation for deleting a task
+    }
+
+    // List Tasks
+    public void listTasks() {
+        // Implementation for listing tasks
+    }
+
+    // Search Task
+    public void searchTask() {
+        // Implementation for searching a task
+    }
+
+    // Filter Task
+    public void filterTask() {
+        // Implementation for filtering tasks
+    }
+
+}
