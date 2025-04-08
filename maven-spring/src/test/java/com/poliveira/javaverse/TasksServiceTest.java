840f269db59292b8be8dfa5351fd5afe19b9659d
Paulo Oliveira <poliveira89@users.noreply.github.com>
2025-04-08 17:09:28 +0100

    crud: improve CRUD for task manager

--- maven-spring/src/test/java/com/poliveira/javaverse/TasksServiceTest.java
+++ maven-spring/src/test/java/com/poliveira/javaverse/TasksServiceTest.java
@@ -11,7 +11,8 @@ import com.poliveira.javaverse.model.SimpleTaskVO;
 import com.poliveira.javaverse.model.TaskVO;
 import com.poliveira.javaverse.repository.TaskRepository;
 import com.poliveira.javaverse.service.TaskService;
-import org.junit.jupiter.api.BeforeEach;
+import java.util.List;
+import java.util.Optional;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.InjectMocks;
@@ -22,15 +23,7 @@ import org.mockito.junit.jupiter.MockitoExtension;
 public class TasksServiceTest {
 
   @Mock private TaskRepository taskRepository;
-
   @InjectMocks private TaskService taskService;
-
-  @BeforeEach
-  public void setUp() {
-    SimpleTaskVO task =
-        SimpleTaskVO.builder().title("Test Task").description("This is a test task").build();
-    taskService.createTask(task);
-  }
 
   @Test
   public void testCreateTask() {
@@ -53,5 +46,55 @@ public class TasksServiceTest {
     assertEquals(task.getTitle(), taskVO.getTitle());
     assertEquals(task.getDescription(), taskVO.getDescription());
     assertEquals(TODO, taskVO.getStatus());
+  }
+
+  @Test
+  public void testFindAll() {
+    TaskVO task =
+        TaskVO.builder()
+            .id(1L)
+            .title("Test Task")
+            .description("This is a test task")
+            .status(TODO)
+            .createdAt(currentTimeMillis())
+            .updatedAt(currentTimeMillis())
+            .build();
+
+    when(taskRepository.findAll()).thenReturn(List.of(task));
+
+    List<TaskVO> tasks = taskService.getAllTasks();
+    assertEquals(1, tasks.size());
+    assertEquals(task.getTitle(), tasks.getFirst().getTitle());
+  }
+
+  @Test
+  public void testUpdateTask() {
+    TaskVO mockExistingTask =
+        TaskVO.builder()
+            .id(1L)
+            .title("Old Task")
+            .description("This is an old task")
+            .status(TODO)
+            .createdAt(currentTimeMillis())
+            .updatedAt(currentTimeMillis())
+            .build();
+    TaskVO mockUpdatedTask =
+        TaskVO.builder()
+            .id(1L)
+            .title("Updated Task")
+            .description("This is an updated task")
+            .status(TODO)
+            .createdAt(currentTimeMillis())
+            .updatedAt(currentTimeMillis())
+            .build();
+
+    when(taskRepository.findById(1L)).thenReturn(Optional.of(mockExistingTask));
+    when(taskRepository.save(any(TaskVO.class))).thenReturn(mockUpdatedTask);
+
+    SimpleTaskVO task =
+        SimpleTaskVO.builder().title("Updated Task").description("This is an updated task").build();
+    TaskVO updatedTask = taskService.updateTask(1L, task);
+    assertEquals(task.getTitle(), updatedTask.getTitle());
+    assertEquals(task.getDescription(), updatedTask.getDescription());
   }
 }
